1bffd7b0aa1a00a61bd58be63f43c59141421580
flear <huseynteymurzade@marun.edu.tr>
2025-07-28 13:19:01 +0300

    game ending screen and background

--- main.lua
+++ main.lua
@@ -16,13 +16,31 @@ local game = {
     levels = {15, 30, 45, 60, 75},
 }
 
+local fonts = {
+    medium = {
+        font = love.graphics.newFont(18),
+        size = 18,
+    },
+    large = {
+        font = love.graphics.newFont(25),
+        size = 25,
+    },
+    gigantic = {
+        font = love.graphics.newFont(45),
+        size = 45,
+    },
+}
+
 local player = Player() 
 
 local buttons = {
-    menu_state = {}
+    menu_state = {},
+    game_over = {}
 }
 
 local enemies = {}
+
+local background
 
 local function changeGameState(state)
     game.state.menu = state == "menu"
@@ -42,13 +60,23 @@ local function startNewGame()
     }
 end
 
+local function goMenu()
+    changeGameState("menu")
+    game.points = 0
+    enemies = {}
+end
+
 function love.mousepressed(x, y, button)
     if not game.state.running then
         if button == 1 then
             if game.state.menu then
                 for index in pairs(buttons.menu_state) do
                     buttons.menu_state[index]:on_mouse_pressed(x, y, button)
-                end 
+                end
+            elseif game.state.gameover then
+                 for index in pairs(buttons.game_over) do
+                    buttons.game_over[index]:on_mouse_pressed(x, y, button)
+                 end
             end
         end
     end
@@ -58,11 +86,15 @@ function love.load()
     love.mouse.setVisible(false)
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    background = love.graphics.newImage("assets/OrbRescueBackground.png")
+
     buttons.menu_state.play_game = button("Play Game", startNewGame, nil, 200, 50)
     buttons.menu_state.settings = button("Settings", nil, nil, 200, 50)
     buttons.menu_state.exit_game = button("Quit Game", love.event.quit, nil, 200, 50)
 
-    
+    buttons.game_over.replay_game = button("Try again", startNewGame, nil, 200, 50)
+    buttons.game_over.menu = button("Menu", goMenu, nil, 200, 50)
+    buttons.game_over.exit_game = button("Quit Game", love.event.quit, nil, 200, 50)
 
 end
 
@@ -83,23 +115,32 @@ function love.update(dt)
                     end
                 end
                 else
-                    changeGameState("menu")
+                    changeGameState("gameover")
                 end
             end
-    
+            game.points = game.points + dt
         end
-        game.points = game.points + dt
+        
     end
     
 
 
 function love.draw()
+
+    if background then
+        local scaleX = love.graphics.getWidth() / background:getWidth()
+        local scaleY = love.graphics.getHeight() / background:getHeight()
+        love.graphics.draw(background, 0, 0, 0, scaleX, scaleY)
+    end
+
+    love.graphics.setFont(fonts.medium.font)
+
     love.graphics.setColor(108/255,56/255,245/255)
-    love.graphics.print("FPS: "..love.timer.getFPS(), love.graphics.newFont(18), love.graphics.getWidth() - 100, love.graphics.getHeight() - 35)
+    love.graphics.print("FPS: "..love.timer.getFPS(), fonts.medium.font, love.graphics.getWidth() - 100, love.graphics.getHeight() - 35)
     
     if game.state.running then
 
-        love.graphics.printf("Points: " .. math.floor(game.points), love.graphics.newFont(25), 10, 10, love.graphics.getWidth(), "left")
+        love.graphics.printf("Points: " .. math.floor(game.points), fonts.large.font, 10, 10, love.graphics.getWidth(), "left")
         for i = 1, #enemies do
             enemies[i]:draw()
         end
@@ -108,6 +149,15 @@ function love.draw()
         buttons.menu_state.play_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.settings.width / 2, love.graphics.getHeight() / 2 - 60)
         buttons.menu_state.settings:draw(love.graphics.getWidth() / 2 - buttons.menu_state.settings.width / 2, love.graphics.getHeight() / 2 + 15)
         buttons.menu_state.exit_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.exit_game.width / 2, love.graphics.getHeight() / 2 + 90)
+    elseif game.state.gameover then
+        love.graphics.setFont(fonts.large.font)
+
+        buttons.game_over.replay_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.settings.width / 2, love.graphics.getHeight() / 2 - 60)
+        buttons.game_over.menu:draw(love.graphics.getWidth() / 2 - buttons.menu_state.settings.width / 2, love.graphics.getHeight() / 2 + 15)
+        buttons.game_over.exit_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.exit_game.width / 2, love.graphics.getHeight() / 2 + 90)
+
+        love.graphics.setColor(108/255,56/255,245/255)
+        love.graphics.printf(math.floor(game.points), fonts.gigantic.font, 0, love.graphics.getHeight() / 2 - fonts.gigantic.size - 160 , love.graphics.getWidth(), "center")
     end
 
     if not game.state.running then
